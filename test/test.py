2daae8b79651900f1c2f2e3eb7d543351daec060
qiang523 <980479090@qq.com>
2021-07-24 09:41:10 +0800

    second commit

--- test/test.py
+++ test/test.py
@@ -2,14 +2,14 @@ from contextlib import closing
 import requests
 from urllib.request import urlretrieve
 
-download_header = {
-    'Referer': 'https://www.dmzj.com/view/yaoshenji/41917.html'
-}
-dn_url = 'https://images.dmzj.com/img/chapterpic/3059/14237/14395217739069.jpg'
-with closing(requests.get(dn_url, headers=download_header, stream=True)) as response:
-    with open('1.jpg', "wb") as file:
-        for data in response.iter_content():
-            file.write(data)
+# download_header = {
+#     'Referer': 'https://www.dmzj.com/view/yaoshenji/41917.html'
+# }
+# dn_url = 'https://images.dmzj.com/img/chapterpic/3059/14237/14395217739069.jpg'
+# with closing(requests.get(dn_url, headers=download_header, stream=True)) as response:
+#     with open('1.jpg', "wb") as file:
+#         for data in response.iter_content():
+#             file.write(data)
 
 
 
